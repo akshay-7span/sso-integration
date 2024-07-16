107e4fde808b4c1c04c94e3e2be03dc2f6a51675
Akshay Vadsara <akshay@7span.com>
2024-07-16 13:15:04 +0530

    Initial Commit

---
+++ src/test/java/com/sso_integration/SsoIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
